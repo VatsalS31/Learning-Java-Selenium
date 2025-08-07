c2aaf36b63aa2aca6eac7275f4828bcce086dada
Vatsal Shah <120210435+VatsalS31@users.noreply.github.com>
2025-08-07 12:18:36 +0530

    Create scroll_javascript.java

---
+++ scroll_javascript.java
@@ -0,0 +1,43 @@
+package introduction;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.*;
+public class JavaScript {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/Users/vatsalshah/Downloads/cdriver/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,500)"); //scrolls the page
+		Thread.sleep(3000);
+		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");//scrolls the table inside the page
+		List<WebElement> values=driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+		int sum=0;
+		for(int i=0;i<values.size();i++)
+		{
+			sum+=Integer.parseInt(values.get(i).getText());
+		}
+		System.out.println(sum);
+		String str=driver.findElement(By.cssSelector(".totalAmount")).getText();
+		String[]ls=str.split(":");
+		int total=Integer.parseInt(ls[1].trim());
+		Assert.assertEquals(sum, total);
+		
+		
+		
+
+	}
+
+}
